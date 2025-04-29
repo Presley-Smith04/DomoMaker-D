fd9570893d45a6182ce0c06aae8f71ca0559324b
Presley-Smith04 <presleysmith04@gmail.com>
2025-04-29 16:43:46 -0400

    update

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-//all initial required imports, requirements, etc.
+// all initial required imports, requirements, etc.
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
@@ -14,30 +14,26 @@ const redis = require('redis');
 
 const router = require('./router.js');
 
-
-//port 3000
+// port 3000
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-
-//link to mongoDB/mongoose
+// link to mongoDB/mongoose
 const dbURL = process.env.MONGODB_URI || 'mongodb://127.0.0.1/StatusMaker';
-mongoose.connect(dbURL).catch((err) => {
-    if (err) {
-        console.log('Could not connect to database');
-        throw err;
-    }
-})
 
+mongoose.connect(dbURL).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Could not connect to MongoDB:', err.message);
+});
 
-//connect to redis
+// connect to redis
 const redisClient = redis.createClient({
     url: process.env.REDISCLOUD_URL,
 });
 
-redisClient.on('error', err => console.log('Redis Client Error', err));
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
-
-//redis connections
+// redis connections
 redisClient.connect().then(() => {
     const app = express();
 
@@ -70,16 +66,16 @@ redisClient.connect().then(() => {
 
     router(app);
 
-
-    
-    //404 response
+    // 404 response
     app.use((req, res) => {
         res.status(404).render('404', { title: 'Page Not Found' });
-      });
-
+    });
 
     app.listen(port, (err) => {
-        if (err) { throw err; }
+        if (err) {
+            console.log('Error starting the server:', err);
+            process.exit(1);  // Ensure the process exits on server startup failure
+        }
         console.log(`Listening on port ${port}`);
     });
-})
+});
